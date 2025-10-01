56c1b7ffc544850dada0d789e711a5a09e52f5e0
Andreas With Aspe <awias@dtu.dk>
2025-10-01 16:10:31 +0200

    Removed hardcorded fold_0. In this file. Not in 3DSegRef github, as I mistankenly wrote in the last commit

--- nnunetv2/training/nnUNetTrainer/nnUNetTrainer.py
+++ nnunetv2/training/nnUNetTrainer/nnUNetTrainer.py
@@ -128,8 +128,8 @@ class nnUNetTrainer(object):
             
             
         #HARDCODED!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-        # self.output_folder = join(self.output_folder_base, f'fold_{fold}')
-        self.output_folder = join(self.output_folder_base, f'fold_0_(4)')
+        self.odutput_folder = join(self.output_folder_base, f'fold_{fold}')
+        # self.output_folder = join(self.output_folder_base, f'fold_0_(1)')
         #######################################################################################
 
         self.preprocessed_dataset_folder = join(self.preprocessed_dataset_folder_base,
